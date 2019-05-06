da60ce1666bcac389c44bbea350d83d31ecd6bad
Jesusmaster <armandodejesus678@gmail.com>
2019-05-06 12:34:44 -0500

    agregando model profile

---
+++ api/core/models/Profile.php
@@ -0,0 +1,47 @@
+<?php
+//profile model
+
+class Profile extends Model {
+
+  private $id;
+  private $name;
+  private $pictures;
+  private $mail;
+  private $password;
+  private $interactions;
+
+  // s e t e r s
+  public function set_id ($id){
+    $this->id = (int) $id;
+  }
+  public function set_name ($name){
+    $this->name = $this->Connect->prepare($name);
+  }
+  public function set_pictures ($pictures){
+    $this->pictures = $this->Connect->prepare(json_encode($pictures));
+  }
+  public function set_mail ($mail){
+    $this->mail = $this->Connect->prepare($mail);
+  }
+  public function set_password ($password){
+    $this->password = $this->Connect->prepare($password);
+  }
+  public function set_interactions ($interactions){
+    $this->interactions = (int) $interactions;
+  }
+
+  public function get_single(){
+    $data = $this->Connect->fetch("SELECT * FROM profile WHERE id = $this->id LIMIT 1");
+    if (!$data) return false;
+    $data['pictures_'] = json_decode($data['pictures']);
+    return $data;
+  }
+
+  public function create(){
+    $id = $this->Connect->create("INSERT INTO profile (name, pictures, interactions) VALUES ('$this->name', '$this->pictures', $this->interactions)");
+    return $id;
+  }
+
+
+
+}
